bfe00ac49c05a2cb1dd0d93f88cf7cbdad0b6a51
IrynaFomina <Irinka9183>
2019-05-29 21:24:43 +0300

    hw18

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,4 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -21,45 +18,43 @@ public class Parser {
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(is);
 
-        Element rootElement = document.getDocumentElement();
-        NodeList nodeList = rootElement.getChildNodes();
+        NodeList nodeList = document.getDocumentElement().getElementsByTagName("event");
         for (int i = 0; i < nodeList.getLength(); i++) {
-            if (nodeList.item(i).getNodeName() == "event") {
                 eventList.add(createEvent(nodeList.item(i)));
-            }
-            System.out.println(nodeList.item(i).getNodeName());
         }
-
         System.out.println(eventList);
     }
 
     private static Event createEvent(Node eventNode) {
         NodeList nodeList = eventNode.getChildNodes();
         Event event = new Event();
+
         for (int i = 0; i < nodeList.getLength(); i++) {
-            switch (nodeList.item(i).getNodeName()){
-                case "event_id" :
-                    event.setSource(nodeList.item(i).getNodeValue());
-                    break;
-                case "event_date" :
-                    event.setEvent_date(nodeList.item(i).getNodeValue());
-                    break;
-                case "event_parameters":
-                    NodeList params = nodeList.item(i).getChildNodes();
-                    for (int j = 0; j < params.getLength(); j++) {
-                        switch (nodeList.item(i).getNodeName()){
-                            case "priority" :
-                                event.setPriority(Integer.parseInt(params.item(i).getNodeValue()));
-                                break;
-                            case "log_level":
-                                event.setLog_level(params.item(i).getNodeValue());
-                                break;
-                            case "source":
-                                event.setSource(params.item(i).getNodeValue());
-                                break;
+            if (nodeList.item(i) instanceof Element) {
+                switch (nodeList.item(i).getNodeName()) {
+                    case "event_id":
+                        event.setEvent_id(nodeList.item(i).getTextContent());
+                        break;
+                    case "event_date":
+                        event.setEvent_date(nodeList.item(i).getTextContent());
+                        break;
+                    case "event_parameters":
+                        NodeList params = nodeList.item(i).getChildNodes();
+                        for (int j = 0; j < params.getLength(); j++) {
+                            switch (params.item(j).getNodeName()) {
+                                case "priority":
+                                    event.setPriority(Integer.parseInt(params.item(j).getTextContent()));
+                                    break;
+                                case "log_level":
+                                    event.setLog_level(params.item(j).getTextContent());
+                                    break;
+                                case "source":
+                                    event.setSource(params.item(j).getTextContent());
+                                    break;
+                            }
                         }
-                    }
-                    break;
+                        break;
+                }
             }
 
         }
